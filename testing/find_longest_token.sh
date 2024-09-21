8a415a1b41135a5c4591884e1142e71c292bd8c4
Athena Boose <pestpestthechicken@yahoo.com>
2024-09-20 19:26:16 -0700

    Edited comments in testing scripts because I'm an idiot and directly copy-pasted them.

--- testing/find_longest_token.sh
+++ testing/find_longest_token.sh
@@ -1,4 +1,5 @@
 # finds all tokenizable strings, gets their length, and puts those lengths into token_lengths.txt
+# also prints the length of the longest token
 # shell scripts are cursed
 grep "<accessible>" ../tokens/8X.xml | sed -e 's/^[ 	]*<accessible>//' | sed -e 's/<\/accessible>$//' | awk '{print length}' > token_lengths.txt
 sort -nr token_lengths.txt | head -n 1
--- testing/find_models.sh
+++ testing/find_models.sh
@@ -1,4 +1,3 @@
-# finds all tokenizable strings, gets their length, and puts those lengths into token_lengths.txt
-# shell scripts are cursed
+# finds all calculator models, sorts them, filters for unique ones, and puts the output in models.txt
 grep "<model>" ../tokens/8X.xml | sed -e 's/^[ 	]*<model>//' | sed -e 's/<\/model>$//' | sort -u > models.txt
 #sort -nr token_lengths.txt | head -n 1
