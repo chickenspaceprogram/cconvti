946c5beb91cdd922e5bba73608d0d26d865c58e5
Athena Boose <pestpestthechicken@yahoo.com>
2024-09-23 12:35:11 -0700

    Mostly implemented the hashmap, still need to implement hash_map_get.

--- token-tools/hashmap.c
+++ token-tools/hashmap.c
@@ -6,46 +6,191 @@
  * Arguments:
  * `str` : The null-terminated string to hash
  */
-uint64_t hash_string(char *str);
+uint64_t hash_(char *str);
+
+/**
+ * An internal function that sets a key-value pair without checking for constraints like the size of the hashmap.
+ * Returns a pointer to the key-value struct.
+ * `array` : A pointer to the hashmap array the key-value pair should be added to
+ * `array-size` : The size of the hashmap array the key-value pair should be added to
+ * `key` : A null-terminated string to be used as key.
+ * `data` : A pointer to the value to be stored in the hashmap.
+ * `data_size` : The size of the data to be added to the hashmap.
+ */
+value *hm_set_internal_(value *array, size_t array_size, char *key, void *data, size_t data_size);
+
+/**
+ * An internal function that gets the key-value pair associated with a key.
+ * Returns a pointer to the key-value pair.
+ * `array` : A pointer to the hashmap array that contains the key.
+ * `key` : The key
+ */
+value *hm_get_internal_(value *array, size_t array_size, char *key);
+
+/**
+ * Doubles the size of the hashmap.
+ * If memory could not be allocated, returns NULL.
+ * Otherwise, returns a pointer to the newly-allocated hashmap.
+ */
+value *hm_double_size_(hash_map *map);
+
+/**
+ * Allocates a new hashmap array of size `size`.
+ * Does not check for a NULL pointer.
+ */
+value *allocate_array_(size_t size);
+
+
 
 /* Definitions */
 
-hash_map *create_hash_map() {
+struct value {
+    void *data;
+    char *key;
+};
+
+struct hash_map {
+    value *hash_array;
+    size_t num_elements;
+    size_t array_size;
+    size_t element_size;
+};
+
+hash_map *hash_map_init(size_t element_size) {
     hash_map *map = malloc(sizeof(hash_map));
     if (map == NULL) {
         return NULL;
     }
-    (*map).hash_array = malloc(ARRAY_DEFAULT_SIZE * sizeof(value));
-    if ((*map).hash_array == NULL) {
+
+    map->element_size = element_size;
+    map->array_size = ARRAY_DEFAULT_SIZE;
+    map->num_elements = 0;
+
+    map->hash_array = allocate_array_(map->array_size);
+    if (map->hash_array == NULL) {
+        free(map);
+        map = NULL;
         return NULL;
     }
-
-    for (int i = 0; i < ARRAY_DEFAULT_SIZE; ++i) {
-        (*map).hash_array[i] = (value) {NULL, NULL, NULL}; // setting all of the pointers in the array to NULL
-    }
-
-    (*map).hash_map_size = ARRAY_DEFAULT_SIZE;
-    (*map).num_elements = 0;
     return map;
 }
 
-// does not deal with resizing hashmap currently
-void *hash_map_add(hash_map map, char *key, void *data) {
-
-    uint64_t index = hash_string(key) & (ARRAY_DEFAULT_SIZE - 1);
-    value *current_index = &(map.hash_array[index]); // variable names are hard ok
-    while ((*current_index).key != NULL) {
-        current_index = (*current_index).next;
+void hash_map_free(hash_map *map) {
+    for (int i = 0; i < map->array_size; ++i) {
+        free(map->hash_array[i].key);
+        free(map->hash_array[i].data);
+        map->hash_array[i].key = NULL;
+        map->hash_array[i].data = NULL;
     }
-    (*current_index).key = malloc(strlen(key) + 1);
-    if ((*current_index).key == NULL) {
-        return NULL;
-    }
-    strcpy((*current_index).key, key);
-
+    free(map->hash_array);
+    free(map);
+    map->hash_array = NULL;
+    map = NULL;
 }
 
-uint64_t hash_string(char *str) {
+void *hash_map_set(hash_map *map, char *key, void *data) {
+    if ((map->num_elements / map->array_size) > MAX_FILL_LEVEL) {
+        value *new_array = hm_double_size_(map);
+        if (new_array == NULL) {
+            hash_map_free(map);
+            return NULL;
+        }
+    }
+    value *valptr = hm_set_internal_(map->hash_array, map->array_size, key, data, map->element_size);
+    if (valptr == NULL) {
+        hash_map_free(map);
+        return NULL;
+    }
+}
+
+void *hash_map_get(hash_map *map, char *key) {
+    
+}
+
+void hash_map_remove(hash_map *map, char *key) {
+    size_t index = hash_(key) & (map->array_size - 1);
+    size_t initial_index = index;
+    while ((map->hash_array[index].key != NULL) && (strcmp(map->hash_array[index].key, key) != 0)) {
+        ++index;
+        if (index == initial_index) {
+            return;
+        }
+    }
+    free(map->hash_array[index].key);
+    free(map->hash_array[index].data);
+    map->hash_array[index].key = NULL;
+    map->hash_array[index].data = NULL;
+}
+
+size_t hash_map_size(hash_map *map) {
+    return map->num_elements;
+}
+
+// only call this when it is safe to do so! this function assumes that there is space in the hashmap.
+value *hm_set_internal_(value *array, size_t array_size, char *key, void *data, size_t data_size) {
+
+    size_t index = hash(key) & (array_size - 1); // binary AND is used instead of modulus because n % 2^k == n & (2^k - 1)
+    size_t key_length = strlen(key);
+
+    while (array[index].key != NULL) {
+        if (strcmp(key, array[index].key) == 0) {
+            array[index].data = data;
+            return;
+        }
+
+        ++index;
+        index = index & (array_size - 1); // keeps the index within the bounds of the table
+    }
+    array[index].key = malloc(sizeof(char) * (key_length + 1));
+    if (array[index].key == NULL) {
+        return NULL;
+    }
+    strcpy(array[index].key, key);
+    array[index].key[key_length] = 0x00;
+
+    array[index].data = malloc(data_size);
+    if (array[index].data == NULL) {
+        free(array[index].key);
+        array[index].key = NULL;
+        return NULL;
+    }
+    memcpy(array[index].data, data, data_size);
+    return &(array[index]);
+}
+
+value *hm_get_internal_(value *array, size_t array_size, char *key) {
+    
+}
+
+// need to handle this returning NULL!
+value *hm_double_size_(hash_map *map) {
+    value *new_array = allocate_array_(map->array_size * 2);
+    if (new_array == NULL) {
+        hash_map_free(map);
+        return NULL;
+    }
+    size_t new_array_size = map->array_size * 2;
+
+    for (int i = 0; i < map->array_size; ++i) {
+        if (map->hash_array[i].key != NULL) {
+            _hm_set(new_array, new_array_size, map->hash_array[i].key, map->hash_array[i].data);
+        }
+    }
+    free(map->hash_array);
+    map->hash_array = new_array;
+    map->array_size = new_array_size;
+    return new_array;
+}
+
+value *allocate_array_(size_t size) {
+    value *array = malloc(size * sizeof(value));
+
+    for (int i = 0; i < ARRAY_DEFAULT_SIZE; ++i) {
+        array[i] = (value) {NULL, NULL}; // setting all of the pointers in the array to NULL
+    }
+}
+
+uint64_t hash_(char *str) {
     int i = 0;
     uint64_t hash = FNV_OFFSET_BASIS;
 
--- token-tools/hashmap.h
+++ token-tools/hashmap.h
@@ -3,7 +3,8 @@
 
 #define FNV_OFFSET_BASIS 0xcbf29ce484222325
 #define FNV_PRIME 0x100000001b3
-#define ARRAY_DEFAULT_SIZE 1024 // must be a power of 2
+#define ARRAY_DEFAULT_SIZE 16 // must be a power of 2
+#define MAX_FILL_LEVEL 0.5
 
 #include <stdio.h>
 #include <stdint.h>
@@ -13,33 +14,43 @@
 
 typedef struct value value;
 
-struct value {
-    value *next;
-    void *data;
-    char *key;
-};
-
 typedef struct hash_map hash_map;
-
-struct hash_map {
-    value *hash_array;
-    unsigned int num_elements;
-    unsigned int hash_map_size;
-};
 
 /**
  * Creates a hash_map with the default sizes.
  * Returns a pointer to the hash_map, or NULL if there was an error.
  */
-hash_map *create_hash_map();
+hash_map *hash_map_init(size_t element_size);
 
 /**
+ * Frees the hashmap at `map`.
+ */
+void hash_map_free(hash_map *map);
+
+/**
+ * Returns a pointer to the data in the hashmap, or NULL if an error occurred.
  * Arguments:
+ * `map` : A pointer to the map the key/value pair should be added to.
  * `key` : A null-terminated string to be used as key.
  * `data` : A pointer to the value to be stored in the hashmap.
- * Returns a pointer to the data in the hashmap.
+ * A deepcopy of both `key` and `data` are stored in the hashmap.
  */
-void *hash_map_add(hash_map map, char *key, void *data);
+void *hash_map_set(hash_map *map, char *key, void *data);
 
+/**
+ * Returns a pointer to the value that `key` maps to in the hashmap `map`.
+ */
+void *hash_map_get(hash_map *map, char *key);
+
+/**
+ * Removes `key` and its associated value from `map`.
+ * If `key` is not in `map`, nothing is done.
+ */
+void hash_map_remove(hash_map *map, char *key);
+
+/**
+ * Returns the number of elements in the hashmap.
+ */
+size_t hash_map_size(hash_map *map);
 
 #endif
