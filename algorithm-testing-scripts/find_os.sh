843d311060878106b0ca33e03fbb560536159c23
Athena Boose <pestpestthechicken@yahoo.com>
2024-09-28 21:55:28 -0700

    Fixed bug in one of the testing shell scripts.

--- algorithm-testing-scripts/find_os.sh
+++ algorithm-testing-scripts/find_os.sh
@@ -1,3 +1,3 @@
 # finds all os versions, sorts them, removes duplicates, and puts the output in os-versions.txt
-grep "<os-version>" tokens/8X.xml | sed -e 's/^[ 	]*<os-version>//' | sed -e 's/<\/os-version>$//' | sort -u > os-versions.txt
+grep "<os-version>" ../tokens/8X.xml | sed -e 's/^[ 	]*<os-version>//' | sed -e 's/<\/os-version>$//' | sort -u > os-versions.txt
 #sort -nr token_lengths.txt | head -n 1
